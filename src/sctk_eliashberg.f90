20955bde5ceb9bc06c3756bdc40a305ddbceb63e
Mitsuaki Kawamura <kawamitsuaki@gmail.com>
2023-12-08 18:09:36 +0900

    Compute number of electrons to prepare charge self-consistency

--- src/sctk_eliashberg.f90
+++ src/sctk_eliashberg.f90
@@ -13,16 +13,19 @@ PROGRAM sctk_eliashberg
   USE sctk_val, ONLY : nmf, ne, mf
   USE sctk_eliashberg_sub, ONLY : xi, dos, temp, omega_pl, omega_ph, lambda, mu_0, mu_c, dosf
   USE sctk_broyden, ONLY: broyden_gapeq
+  USE klist, ONLY : nelec
+  USE ener, ONLY : ef
   !
   IMPLICIT NONE
   !
   INTEGER :: ie, imf
-  REAL(DP) :: ef, ms, emax, res_chi, res_phi, res_Z, Vuc
+  REAL(DP) :: ms, emax, Vuc, nelec1, nelec2
   REAL(DP),ALLOCATABLE :: Zcp(:,:)
   !
   Vuc = 110.5871_dp ! a.u.^3
   ef = 0.824_dp ! Ry
   ms = 1.05 !
+  nelec = 3.0_dp
   dosf = 2.74834984056_dp ! /Ry/cell/spin
   lambda = 0.402_dp * 2.0_dp
   mu_0 = 0.25070_dp
@@ -53,8 +56,26 @@ PROGRAM sctk_eliashberg
     dos(ie) = SQRT(ms**3*(xi(ie)+ef)) / pi**2 * 0.25_dp * Vuc
   END DO
   !
+  nelec1 = 0.0_dp
+  nelec2 = 0.0_dp
+  DO ie = 1, ne - 1
+    !
+    nelec1 = nelec1 + 0.5_dp *(xi(ie+1)-xi(ie)) &
+    &               * ( dos(ie  )*(1.0_dp-TANH(0.5_dp*xi(ie  )/temp)) &
+    &                 + dos(ie+1)*(1.0_dp-TANH(0.5_dp*xi(ie+1)/temp)) )
+    !
+    nelec2 = nelec2 + 0.5_dp * (xi(ie+1)-xi(ie)) * (dos(ie  ) + dos(ie+1))
+    !
+    DO imf = 1, nmf
+      nelec2 = nelec2 - 0.5_dp *(xi(ie+1)-xi(ie)) * 2.0_dp * temp &
+      &               * ( dos(ie  )*(xi(ie  )/(mf(imf)**2+xi(ie  )**2)) &
+      &                 + dos(ie+1)*(xi(ie+1)/(mf(imf)**2+xi(ie+1)**2)) )
+    END DO
+  END DO
+  WRITE(*,*) "debug1", nelec1, nelec2
+  !
   Zcp(1:nmf,1) = 1.0_dp
-  Zcp(1:nmf,2) = 1.0_dp
+  Zcp(1:nmf,2) = 0.0_dp
   Zcp(1:nmf,3) = 1.0_dp
   !
   OPEN(200, file="eliashberg.dat")
