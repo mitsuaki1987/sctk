3412b14b387a653b13562c47d24d2e27cbe8cbc0
Mitsuaki Kawamura <kawamitsuaki@gmail.com>
2025-02-04 18:21:51 +0900

    Normalize after symmetrize to stabilize

--- src/sctk_ini_delta.f90
+++ src/sctk_ini_delta.f90
@@ -262,9 +262,10 @@ SUBROUTINE compute_dosk(dos, dxq_dos)
   !
   CALL mp_sum(dos,     world_comm)
   CALL mp_sum(dxq_dos, world_comm)
-  dxq_dos(:,:,:) = dxq_dos(:,:,:) / dos(:,:,:)
   !
   CALL symm_dosk(dos, dxq_dos)
+  !
+  dxq_dos(:,:,:) = dxq_dos(:,:,:) / dos(:,:,:)
   !
 END SUBROUTINE compute_dosk
 !
